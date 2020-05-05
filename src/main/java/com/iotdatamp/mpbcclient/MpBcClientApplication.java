65ff138225f7a6df286b82b1f77bd581426756a0
ddanijel <danijel.fon@gmail.com>
2020-05-05 19:41:39 +0200

    project init

---
+++ src/main/java/com/iotdatamp/mpbcclient/MpBcClientApplication.java
@@ -0,0 +1,13 @@
+package com.iotdatamp.mpbcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpBcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpBcClientApplication.class, args);
+    }
+
+}
